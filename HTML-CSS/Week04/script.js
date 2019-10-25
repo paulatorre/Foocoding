0262fb2c249a4043f57f45b685edd1c0ff8153f1
Pau Latorre <55534943+paulatorre@users.noreply.github.com>
2019-10-25 22:00:16 +0200

    Homework w04

---
+++ HTML-CSS/Week04/script.js
@@ -0,0 +1,7 @@
+/*function menuMobile(){
+    const burger = document.querySelectorAll('.burger')
+    burger.forEach(nav => nav.classList.toggle('nav-active'));
+}
+document.querySelector(nav).addEventListener('click', menuMobile);*/
+
+
